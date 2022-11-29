d452900fc7774672359c70ddc1302ffe83d35354
ayaka tooya <na081543@icloud.com>
2022-11-29 23:05:26 +0900

    jQuery使用できるように設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_29_132424) do
+ActiveRecord::Schema.define(version: 2022_11_29_135901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 2022_11_29_132424) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "team_id"
     t.index ["team_id"], name: "index_kids_on_team_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.date "day"
+    t.text "remark"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "teams", force: :cascade do |t|
